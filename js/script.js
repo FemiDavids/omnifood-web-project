eb427761f8f3d7317b78cd78fc9dc88dc5ccdab4
David Onuoha <thefemidavids@gmail.com>
2024-02-09 19:44:42 +0100

    js commit

--- js/script.js
+++ js/script.js
@@ -54,6 +54,33 @@ allLinks.forEach(function (link) {
 });
 
 /////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+/////////////////////////////////////////////////////
 // fixing flexbox gap property missing in safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
